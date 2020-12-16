3fd8d9dac6c507ab72352738f5955a0f4004a028
415CA <chrisallen415@gmail.com>
2020-12-16 13:00:05 -0500

    added users and messages GET routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+// API Data 
+import users from './Users';
+import messages from './Messages';
+
+// Port
 const PORT = process.env.PORT; 
 
+//Initialize Express
 const app = express();
 
+// Enable Cors
 app.use(cors());
 
+// RESTful Routes
 app.get('/users', (request, response) => {
-  return response.send('Received a GET HTTP method on user resource');
+  return response.send(Object.values(users));
+});
+
+app.get('/users/:userId', (request, response) => {
+  return response.send(users[request.params.userId]);
 });
 
 app.post('/users', (request, response) => {
@@ -28,6 +40,15 @@ app.delete('/users/:userID', (request, response) => {
   );
 });
 
+app.get('/messages', (request, response) => {
+  return response.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (request, response) => {
+  return response.send(messages[request.params.messageId]);
+});
+
+// Enable App Server
 app.listen(PORT, () =>
   console.log(`Example app listening on port ${PORT}!`),
 );
